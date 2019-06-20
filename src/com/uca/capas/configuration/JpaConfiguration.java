23913772333316c08c9071a6c6aa9e18d56e0464
Kathgo97 <00011616@uca.edu.sv>
2019-06-20 16:43:50 -0700

    Se creo la vista de inicio de sesion

--- src/com/uca/capas/configuration/JpaConfiguration.java
+++ src/com/uca/capas/configuration/JpaConfiguration.java
@@ -43,7 +43,7 @@ public class JpaConfiguration {
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/Biblioteca");
+		dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/tareaNCapas");
 		dataSource.setUsername("postgres");
 		dataSource.setPassword("root");
 		return dataSource;
